360f8c90b53da94a0deac1fb0c896f97e048c785
Mindey <mindey@wefindx.lan>
2018-03-09 23:07:05 +0000

    add schema

--- tasks/wechat.py
+++ tasks/wechat.py
@@ -10,13 +10,56 @@ class Task(object):
     name = 'wechat'
     schema_name = 'wechat.org/message'
     schema_version = 'schema-version=1.0.0'
-    schema_source_url = 'https://wechat.org/unknown/'
+    schema_source_url = 'https://www.wikidata.org/wiki/Q628523'
 
     schema = {
-        'date': {'': [['str'], []]},
-        'description': {'': [['str'], []]},
-        'link': {'': [['str'], []]},
-        'title': {'': [['str'], []]}
+      'AppInfo': {
+          'AppID': {'': [['str'], ['https://www.wikidata.org/wiki/Q50379926']]},
+          'Type': {'': [['int'], ['https://www.wikidata.org/wiki/Q16889133']]}},
+      'AppMsgType': {'': [['int'], ['https://www.wikidata.org/wiki/Q628523', 'https://www.wikidata.org/wiki/Q16889133']]},
+      'Content': {'': [['str'], []]},
+      'CreateTime': {'': [['int'], []]},
+      'EncryFileName': {'': [['str'], []]},
+      'FileName': {'': [['str'], []]},
+      'FileSize': {'': [['str'], []]},
+      'ForwardFlag': {'': [['int'], []]},
+      'FromUserName': {'': [['str'], []]},
+      'HasProductId': {'': [['int'], []]},
+      'ImgHeight': {'': [['int'], []]},
+      'ImgStatus': {'': [['int'], []]},
+      'ImgWidth': {'': [['int'], []]},
+      'MediaId': {'': [['str'], []]},
+      'MsgId': {'': [['str'], []]},
+      'MsgType': {'': [['int'], []]},
+      'NewMsgId': {'': [['int'], []]},
+      'OriContent': {'': [['str'], []]},
+      'PlayLength': {'': [['int'], []]},
+      'RecommendInfo': {
+          'Alias': {'': [['str'], []]},
+          'AttrStatus': {'': [['int'], []]},
+          'City': {'': [['str'], ['https://www.wikidata.org/wiki/Q515']]},
+          'Content': {'': [['str'], []]},
+          'NickName': {'': [['str'], []]},
+          'OpCode': {'': [['int'], []]},
+          'Province': {'': [['str'], []]},
+          'QQNum': {'': [['int'], ['https://www.wikidata.org/wiki/Q50379921']]},
+          'Scene': {'': [['int'], []]},
+          'Sex': {'': [['int'], ['https://www.wikidata.org/wiki/Q290']]},
+          'Signature': {'': [['str'], []]},
+          'Ticket': {'': [['str'], []]},
+          'UserName': {'': [['str'], []]},
+          'VerifyFlag': {'': [['int'], []]}
+      },
+      'Status': {'': [['int'], []]},
+      'StatusNotifyCode': {'': [['int'], []]},
+      'StatusNotifyUserName': {'': [['str'], []]},
+      'SubMsgType': {'': [['int'], []]},
+      'Text': {'': [['str'], []]},
+      'Ticket': {'': [['str'], []]},
+      'ToUserName': {'': [['str'], []]},
+      'Type': {'': [['str'], []]},
+      'Url': {'': [['str'], []]},
+      'VoiceLength': {'': [['int'], []]}
     }
 
     def __init__(self, loop):
