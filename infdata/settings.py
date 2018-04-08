0af41ae6e6a89417fb22c8f8c78300769be8d3e2
Mindey <mindey@qq.com>
2018-04-08 22:05:17 +0000

    update version

--- infdata/settings.py
+++ infdata/settings.py
@@ -1,9 +1,8 @@
 INITIAL_SERVERS = [
     ('LOCALHOST', 'http://0.0.0.0:8000'),
     ('DEVELOPMENT', 'https://dev.wfx.io'),
-    ('Shanghai', 'https://test.wefindx.io'),
-    ('Vilnius', 'https://lt.wfx.io'),
-    ('Frankfurt', 'https://test.wfx.io')
+    ('Dublin', 'https://wefindx.com.io'),
+    ('Shanghai', 'https://wefindx.io'),
 ]
 
 INITIAL_CURRENT = 'LOCALHOST'
